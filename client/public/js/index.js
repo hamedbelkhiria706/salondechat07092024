bf8a7fe931db7507a7b675f3eaa41141feda7e26
hamedbelkhiria706 <hamedbelkhiria706@gmail.com>
2024-09-18 14:13:51 +0200

    m

--- client/public/js/index.js
+++ client/public/js/index.js
@@ -1,7 +1,12 @@
 // Load the navbar
-fetch("navbar.html")
+/*fetch("navbar.html")
   .then((response) => response.text())
   .then((data) => {
     document.getElementById("navbar-placeholder").innerHTML = data;
   })
   .catch((error) => console.error("Error loading the navbar:", error));
+*/
+
+$(document).ready(function () {
+  $("#content").load("navbar.html");
+});
