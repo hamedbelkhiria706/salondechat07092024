ebe041514c3fb678bb79ad54d36369994bea2d6c
hamedbelkhiria706 <hamedbelkhiria706@gmail.com>
2024-09-28 13:04:32 +0200

    m

--- server/config/db.js
+++ server/config/db.js
@@ -5,7 +5,7 @@ const database1 = "salonchatdboscarf28092024";
 const client = new MongoClient(uri);
 const JWT_SECRET =
   "your_jwt_secretc8a670214aad12c74b6bec703cced22a7c249c3d6df566ca2d1cb4194b493ec6";
-console.log(JWT_SECRET);
+//console.log(JWT_SECRET);
 async function connectDB() {
   await client.connect();
 }
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,6 @@
 const { client, database1 } = require("../config/db"); // Use the correct path to your db.js file
-const router = express.Router();
 const express = require("express");
+const router = express.Router();
 
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
@@ -80,3 +80,4 @@ router.post("/signup", async (req, res) => {
     res.status(500).json({ message: "Error creating user.", error });
   }
 });
+module.exports = router;
