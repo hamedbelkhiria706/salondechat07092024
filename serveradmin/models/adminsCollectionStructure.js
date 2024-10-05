49741405bd641d916354bd868748d7380810dba2
hamedbelkhiria706 <hamedbelkhiria706@gmail.com>
2024-10-05 13:51:15 +0200

    m

--- serveradmin/models/adminsCollectionStructure.js
+++ serveradmin/models/adminsCollectionStructure.js
@@ -1,17 +1,10 @@
 const { JWT_SECRET, client, database1 } = require("../config/db"); // Ensure correct path to your db.js file
 const { ObjectId } = require("mongodb");
-const Joi = require("joi");
+
 const validationSchema = {
   $jsonSchema: {
     bsonType: "object",
-    required: [
-      "email",
-      "username",
-      "password",
-      "isVerified",
-      "maxRooms",
-      "adminRooms",
-    ],
+    required: ["email", "username", "password", "isVerified"],
     properties: {
       _id: {
         bsonType: "objectId",
@@ -23,36 +16,14 @@ const validationSchema = {
       username: {
         bsonType: "string",
         description: "must be a string and is required",
-        pattern: Joi.string().alphanum().min(3).max(30).required(),
       },
       password: {
         bsonType: "string",
         description: "must be a string and is required",
-        pattern: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
       },
       isVerified: {
         bsonType: "bool",
         description: "must be a boolean and is required",
-      },
-      maxRooms: {
-        bsonType: "int",
-        description: "must be an integer and is required",
-      },
-      adminRooms: {
-        bsonType: "array",
-        items: {
-          bsonType: "objectId",
-        },
-        description: "must be an array of ObjectIds and is required",
-      },
-      plan: {
-        type: "object",
-        properties: {
-          type: { type: "string" },
-          date_debut: { type: "string", format: "date-time" },
-          date_fin: { type: "string", format: "date-time" },
-        },
-        required: ["type", "date_debut", "date_fin"],
       },
     },
   },
@@ -63,7 +34,7 @@ let isValidationSetUp = false;
 async function setupValidation() {
   if (!isValidationSetUp) {
     await client.db(database1).runCommand({
-      collMod: "users",
+      collMod: "admins",
       validator: validationSchema,
       validationAction: "error",
     });
@@ -71,7 +42,7 @@ async function setupValidation() {
   }
 }
 
-const usersCollection = client.db(database1).collection("users");
+const usersCollection = client.db(database1).collection("admins");
 
 // Call the setup function to ensure validation is applied
 setupValidation().catch(console.error);
