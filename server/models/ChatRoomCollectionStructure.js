b722e764d0f4d6468679711f79a6eca90dd4ee93
hamedbelkhiria706 <hamedbelkhiria706@gmail.com>
2024-10-06 13:54:10 +0200

    m

---
+++ server/models/ChatRoomCollectionStructure.js
@@ -0,0 +1,8 @@
+const { JWT_SECRET, client, database1 } = require("../config/mongoosedb"); // Use the correct path to your db.js file
+const mongoose = require("mongoose");
+
+// ChatRoom Schema
+const chatRoomSchema = new Schema({
+  name: { type: String, required: true },
+  users: [{ type: mongoose.Schema.Types.ObjectId, ref: "user" }],
+});
---
+++ server/models/MessageCollectionStructure.js
@@ -0,0 +1,19 @@
+const { JWT_SECRET, client, database1 } = require("../config/mongoosedb"); // Use the correct path to your db.js file
+const mongoose = require("mongoose");
+// Message Schema
+
+const messageSchema = new Schema({
+  senderId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  chatRoomld: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "ChatRoom",
+    required: true,
+  },
+  message: { type: String, required: true },
+
+  timestamp: { type: Date, default: Date.now },
+});
