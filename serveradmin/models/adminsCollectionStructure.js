e1c2eb5dcfa6396f9e6bc04f594011e16483d177
hamedbelkhiria706 <hamedbelkhiria706@gmail.com>
2024-10-12 12:00:28 +0200

    m

--- serveradmin/models/adminsCollectionStructure.js
+++ serveradmin/models/adminsCollectionStructure.js
@@ -1,53 +1,33 @@
 const { JWT_SECRET, client, database1 } = require("../config/db"); // Ensure correct path to your db.js file
 const { ObjectId } = require("mongodb");
+const mongoose = require("mongoose");
 
-const validationSchema = {
-  $jsonSchema: {
-    bsonType: "object",
-    required: ["email", "username", "password", "isVerified"],
-    properties: {
-      _id: {
-        bsonType: "objectId",
-      },
-      email: {
-        bsonType: "string",
-        description: "must be a string and is required",
-      },
-      username: {
-        bsonType: "string",
-        description: "must be a string and is required",
-      },
-      password: {
-        bsonType: "string",
-        description: "must be a string and is required",
-      },
-      isVerified: {
-        bsonType: "bool",
-        description: "must be a boolean and is required",
-      },
-    },
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    description: "must be a string and is required",
   },
-};
+  username: {
+    type: String,
+    required: true,
+    description: "must be a string and is required",
+  },
+  password: {
+    type: String,
+    required: true,
+    description: "must be a string and is required",
+  },
+  isVerified: {
+    type: Boolean,
+    required: true,
+    description: "must be a boolean and is required",
+  },
+});
 
-let isValidationSetUp = false;
-
-async function setupValidation() {
-  if (!isValidationSetUp) {
-    await client.db(database1).runCommand({
-      collMod: "admins",
-      validator: validationSchema,
-      validationAction: "error",
-    });
-    isValidationSetUp = true; // Mark validation as set up
-  }
-}
-
-const usersCollection = client.db(database1).collection("admins");
-
-// Call the setup function to ensure validation is applied
-setupValidation().catch(console.error);
+const User = mongoose.model("User", userSchema);
 
 module.exports = {
-  usersCollection,
+  usersCollection: User,
   setupValidation, // Export the setup function if needed elsewhere
 };
