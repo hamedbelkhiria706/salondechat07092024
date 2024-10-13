88f579fa5e6f14933fb0c1b3efbc1e3c8e4c516a
hamedbelkhiria706 <hamedbelkhiria706@gmail.com>
2024-10-13 13:59:12 +0200

    m

--- server/models/usersCollectionStructure.js
+++ server/models/usersCollectionStructure.js
@@ -6,6 +6,8 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  isVerified: { type: Boolean, default: false },
+  verificationToken: { type: String },
   createdAt: { type: Date, default: Date.now },
   subscriptionPlan: {
     type: mongoose.Schema.Types.ObjectId,
