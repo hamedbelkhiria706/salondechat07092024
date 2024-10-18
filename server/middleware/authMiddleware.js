9688813e0a4f19577d914b5cb657477cdd1adc85
hamedbelkhiria706 <hamedbelkhiria706@gmail.com>
2024-10-18 17:57:52 +0200

    m

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -8,6 +8,7 @@ const authMiddleware = async (req, res, next) => {
   ) {
     try {
       token = req.headers.authorization.split(" ")[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
     } catch (e) {}
   }
 };
