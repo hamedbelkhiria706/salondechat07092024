6bd8e7a28a526ec4561e9a83e2e83020842e60ea
hamedbelkhiria706 <hamedbelkhiria706@gmail.com>
2024-10-18 18:11:00 +0200

    m

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -20,3 +20,4 @@ const adminMiddleware = (req, res, next) => {
   if (req.user && req.user.role === "admin") next();
   else res.status(403).json({ message: "Not authorized as admin" });
 };
+module.exports = { authMiddleware, adminMiddleware };
