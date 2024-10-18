4af76048a2c271a89eebc5b15461af32bac539c2
hamedbelkhiria706 <hamedbelkhiria706@gmail.com>
2024-10-18 18:01:04 +0200

    m

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -9,6 +9,10 @@ const authMiddleware = async (req, res, next) => {
     try {
       token = req.headers.authorization.split(" ")[1];
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    } catch (e) {}
+      req.user = await User.findById(decoded.id).select("-password");
+      next();
+    } catch (error) {
+      res.status(401).json({ message: "Not authorized, token failed" });
+    }
   }
 };
