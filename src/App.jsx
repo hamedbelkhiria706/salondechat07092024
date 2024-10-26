e5eabb5b1b386579d8b70712cfaccff69d761dc8
hamedbelkhiria706 <hamedbelkhiria706@gmail.com>
2024-10-26 12:52:40 +0200

    m

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import React from "react";
+import "./styles/style.css"; // Import your existing CSS file here
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello, React with Vite!</h1>
+      {/* Your other HTML content here */}
+    </div>
+  );
+};
+
+export default App;
