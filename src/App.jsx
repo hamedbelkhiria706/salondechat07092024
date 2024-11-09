2aba8b0b4f03cf9891fec14ccf8d31bacf9da185
hamedbelkhiria706 <hamedbelkhiria706@gmail.com>
2024-11-09 12:27:50 +0100

    m

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./Pages/Index";
-
+import Navbar from "./components/newnavbar";
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
       </Routes>
---
+++ src/components/newfooter.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./stylescss/global.css";
+import $ from "jquery";
+
+const footer = () => {
+  return (
+    <footer className="bg-dark text-white mt-5 p-4 text-center">
+      <p>
+        &copy; 2024 Application de Chat en Temps Réel | Tous droits réservés.
+      </p>
+      <p>
+        <a href="politique.html" className="text-white">
+          Politique de confidentialité
+        </a>
+        |{" "}
+        <a href="mentions.html" className="text-white">
+          Mentions légales
+        </a>{" "}
+        |
+        <a href="about.html" className="text-white">
+          Contactez-nous
+        </a>
+      </p>
+    </footer>
+  );
+};
+export default footer;
