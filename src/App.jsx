9a7afd17a859dc01d7bad7b1c51aa0ebea9b807f
hamedbelkhiria706 <hamedbelkhiria706@gmail.com>
2024-11-15 10:57:47 +0100

    m

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./Pages/Index";
-<<<<<<< HEAD
 import ContactPage from "./Pages/Contact";
-
-function App() {
-  return (
-    <Router>
-      {/* <Navbar></Navbar> */}
-=======
 import Navbar from "./components/newnavbar";
+import Footer from "./components/newfooter";
 function App() {
   return (
     <Router>
-      <Navbar />
->>>>>>> 2aba8b0b4f03cf9891fec14ccf8d31bacf9da185
+      <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/contact" element={<ContactPage />} />
       </Routes>
-      {/* <Footer></Footer> */}
+      <Footer></Footer>
     </Router>
   );
 }
