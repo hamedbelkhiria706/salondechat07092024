11074ae712a65f664374388323b98261de06d23e
hamed belkhiria <hamedbelkhiria706@gmail.com>
2024-11-23 12:01:26 +0100

    m

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Navbar from "./components/MainNavbar";
 import Footer from "./components/newfooter";
 import Plans from "./Pages/Plans";
 import Login from "./Pages/AuthPage";
+import MentionsLegales from "./components/MentionsLegales";
+import PolitiqueDeConfidentialite from "./components/PolitiqueDeConfidentialite";
 function App() {
   return (
     <Router>
@@ -15,6 +17,8 @@ function App() {
         <Route path="/contact" element={<ContactPage />} />
         <Route path="/plans" element={<Plans />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/mentionslegales" element={<MentionsLegales/>}/>
+        <Route path="politiquedeconfidentialite" element={<PolitiqueDeConfidentialite/>}/>
       </Routes>
       <Footer></Footer>
     </Router>
