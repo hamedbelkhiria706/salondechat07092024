b3d72c71cde4708fb8eeea9971c883af3855804b
hamed belkhiria <hamedbelkhiria706@gmail.com>
2024-11-28 15:00:12 +0100

    m

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 function Statistics() {
   return (
-    <div>Statistics</div>
+    <div>
+      <h3>Salons de chat crées: 3</h3>
+      <h3>Amis: 4</h3>
+      <h3>Bloqués : 5</h3>
+      <h3>Salons de chat rejoints: 14</h3>
+      <h3>Membership: Gratuit</h3>
+    </div>
   )
 }
 
