f301486fc0554a597f5505300810cdaad12cee52
hamed belkhiria <hamedbelkhiria706@gmail.com>
2024-11-30 12:52:42 +0100

    m

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-
+import {Link} from "react-router-dom"
 function Profile() {
   return (
      
 <main>
 <div className="container mt-5">
 <h1>Profile</h1>
+<Link to="/exportdata">Exporter les données</Link>
 </div>
     </main>
   )
