43be41d0f1a2c08b961e86d24072743323d4883c
hamed belkhiria <hamedbelkhiria706@gmail.com>
2024-11-30 12:54:37 +0100

    m

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -7,6 +7,12 @@ function Profile() {
 <div className="container mt-5">
 <h1>Profile</h1>
 <Link to="/exportdata">Exporter les données</Link>
+<Link to="/editprofile" className="btn btn-secondary mt-3">
+          Editer profil
+        </Link>
+        <Link to="/password" className="btn btn-secondary mt-3">
+          Changer mot de passe
+        </Link>
 </div>
     </main>
   )
