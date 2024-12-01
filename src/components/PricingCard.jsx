d3e95301765a37125ed363693d59d46a2224fb54
hamed belkhiria <hamedbelkhiria706@gmail.com>
2024-12-01 12:17:08 +0100

    m

--- src/components/PricingCard.jsx
+++ src/components/PricingCard.jsx
@@ -4,9 +4,9 @@ const PricingCard = ({ plan, price, description, features, buttonText }) => {
   return (
     <div className="col-md-4 bg-dark">
       <div className="card text-center mb-4 bg-dark">
-        <div className="card-header bg-dark">{plan}</div>
+        <div className="card-header bg-dark text-white">{plan}</div>
         <div className="card-body bg-dark">
-          <h5 className="card-title bg-dark">{price}</h5>
+          <h5 className="card-title bg-dark text-white">{price}</h5>
           <p className="card-text bg-dark">{description}</p>
           <p className="card-text bg-dark">{features}</p>
         </div>
