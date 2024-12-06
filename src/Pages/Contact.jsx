45bd6144cd55e027e51a0e9d8646c8a38db121a2
hamed belkhiria <hamedbelkhiria706@gmail.com>
2024-12-06 16:09:58 +0100

    m

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -31,7 +31,7 @@ const ContactPage = () => {
     }
   };
   return (
-    <main>
+    <main className="bg-dark">
       <header className="mt-5 bg-dark">
         <div className="row justify-content-center">
           <div className="col-md-12">
@@ -39,7 +39,7 @@ const ContactPage = () => {
               <div className="row no-gutters mb-5">
                 <div className="col text-white">
                   <div className="w-100 p-md-5 p-4">
-                    <h1 className="mb-4">Contact Us</h1>
+                    <h1 className="mb-4">Nous contacter</h1>
                     <div id="form-message-warning" className="mb-4"></div>
                     <div id="form-message-success" className="mb-4">
                       Votre message a été envoyé merci!
