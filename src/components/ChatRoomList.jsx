319fe4991136475d1125a0041da9990b99f67c17
unknown <hamedbelkhiria706@gmail.com>
2024-12-17 15:33:13 +0100

    m

--- src/components/ChatRoomList.jsx
+++ src/components/ChatRoomList.jsx
@@ -6,7 +6,9 @@ function ChatRoomList() {
       <h1>
       Liste de chat rooms
       </h1>
-      
+      <h2>Chatroom 1</h2>
+      <h2>Chatroom 2</h2>
+      <h2>Chatroom 3</h2>
       </div>
   )
 }
