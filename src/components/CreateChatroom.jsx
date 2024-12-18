a6ce38a9d858830577f1565c8558a85256ab34b7
unknown <hamedbelkhiria706@gmail.com>
2024-12-18 07:36:02 +0100

    m

--- src/components/CreateChatroom.jsx
+++ src/components/CreateChatroom.jsx
@@ -3,6 +3,18 @@ import React from 'react'
 function CreateChatroom() {
   return (
     <div><h1>Créer un salon de chat</h1>
+    <form>
+    <label htmlFor={`roomName`}>
+                  Nom du Salon  :
+                </label>
+                <input
+                  type="text"
+                  className="form-control room-input"
+                  id={`roomName`}
+                  value={newRooms[index] || ""}
+                  onChange={(e) => handleRoomNameChange(index, e.target.value)}
+                />
+    </form>
     </div>
   )
 }
