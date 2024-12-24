801584919172b6e69e5d1d90b1ab0bdff8558c77
unknown <hamedbelkhiria706@gmail.com>
2024-12-24 15:50:16 +0100

    m

--- src/components/CreateChatroom.jsx
+++ src/components/CreateChatroom.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 function CreateChatroom() {
+  let [newRooms, setNewRooms] = useState([]);
+  let [createdRooms, setCreatedRooms] = useState([]);
+  const handleRoomNameChange = (index, value) => {
+    const updatedRooms = [...newRooms];
+    updatedRooms[index] = value;
+    setNewRooms(updatedRooms);
+  };
+  let index=0
   return (
     <div><h1>Créer un salon de chat</h1>
     <form>
@@ -14,6 +22,7 @@ function CreateChatroom() {
                   value={newRooms[index] || ""}
                   onChange={(e) => handleRoomNameChange(index, e.target.value)}
                 />
+    <button type="button" onClick={()=>alert('ajouté')}>envoi</button>
     </form>
     </div>
   )
