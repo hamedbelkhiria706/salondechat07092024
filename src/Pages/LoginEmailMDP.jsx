2d2e42e50d8d4ed32ba8cd4bc30450d0252c34a7
unknown <hamedbelkhiria706@gmail.com>
2025-01-04 11:42:15 +0100

    m

--- src/Pages/LoginEmailMDP.jsx
+++ src/Pages/LoginEmailMDP.jsx
@@ -3,6 +3,24 @@ import React,{useState} from 'react'
 function LoginEmailMDP() {
     const [email,setEmail]=useState('')
     const [password,setPassword]=useState('')
+    const [isValid, setIsValid] = useState(false);
+    const [error, setError] = useState('');
+    const validatePassword = (password) => {
+        // Regex for password validation
+        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,}$/;
+        
+        if (passwordRegex.test(password)) {
+            setIsValid(true);
+            setError(''); // Clear error message if valid
+        } else {
+            setIsValid(false);
+            setError('Password must be at least 8 characters long and include an uppercase letter, a lowercase letter, a number, and a special character.');
+        }
+      };
+      const handleChange = () => {
+         
+        validatePassword(password);
+      };
     const handleSubmit=(e)=>{
         e.preventDefault();
         console.log(email)
@@ -13,11 +31,18 @@ function LoginEmailMDP() {
         <h1>Connexion par login et mot de passe</h1>
         <form onSubmit={handleSubmit}>
         <h2>Email:</h2>
-        <input type="email" onChange={(e)=>setEmail(e.target.value)}></input>
+        <input type="email" required onChange={(e)=>setEmail(e.target.value)}></input>
         <br/>
         <h2>Mot de passe:</h2>
-        <input type="password" onChange={(e)=>setPassword(e.target.value)}></input>
+        <input type="password" onChange={(e)=>{
+            
+            setPassword(e.target.value);
+            validatePassword(e.target.value);
+        }}  required
+              style={{ borderColor: isValid ? 'green' : 'red' }}
+              ></input>
         <br/>
+        {!isValid && error && <p style={{ color: 'red' }}>{error}</p>}
         <button type="sumbit">Envoyer</button>
         </form>
     </div>
