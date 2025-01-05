2a3e98bb1faab4b694cfedd17dc784b11be7f5a1
unknown <hamedbelkhiria706@gmail.com>
2025-01-05 12:48:04 +0100

    m

--- src/Pages/MentionsLegales.jsx
+++ src/Pages/MentionsLegales.jsx
@@ -33,31 +33,31 @@ function MentionsLegales() {
 
 
 
-                <h2>2. Conditions générales d’utilisation du site et des services proposés.</h2>
+                <h2>2. Conditions générales d'utilisation du site et des services proposés.</h2>
 
-                <p>Le Site constitue une œuvre de l’esprit protégée par les dispositions du Code de la Propriété Intellectuelle et des Réglementations Internationales applicables. 
+                <p>Le Site constitue une œuvre de l'esprit protégée par les dispositions du Code de la Propriété Intellectuelle et des Réglementations Internationales applicables. 
                 Le Client ne peut en aucune manière réutiliser, céder ou exploiter pour son propre compte tout ou partie des éléments ou travaux du Site.</p>
 
-                <p>L’utilisation du site <a href="https://salondechat.com">https://salondechat.com</a> implique l’acceptation pleine et entière des conditions générales d’utilisation ci-après décrites. Ces conditions d’utilisation sont susceptibles d’être modifiées ou complétées à tout moment, les utilisateurs du site <a href="https://salondechat.com">https://salondechat.com</a> sont donc invités à les consulter de manière régulière.</p>
+                <p>L'utilisation du site <a href="https://salondechat.com">https://salondechat.com</a> implique l’acceptation pleine et entière des conditions générales d’utilisation ci-après décrites. Ces conditions d’utilisation sont susceptibles d’être modifiées ou complétées à tout moment, les utilisateurs du site <a href="https://salondechat.com">https://salondechat.com</a> sont donc invités à les consulter de manière régulière.</p>
 
                 <p>Ce site internet est normalement accessible à tout moment aux utilisateurs. Une interruption pour raison de maintenance technique peut être toutefois décidée par <a href="https://salondechat.com">https://salondechat.com</a>, qui s’efforcera alors de communiquer préalablement aux utilisateurs les dates et heures de l’intervention.
                 Le site web <a href="https://salondechat.com">https://salondechat.com</a> est mis à jour régulièrement par <a href="https://salondechat.com">https://salondechat.com</a> responsable. De la même façon, les mentions légales peuvent être modifiées à tout moment : elles s’imposent néanmoins à l’utilisateur qui est invité à s’y référer le plus souvent possible afin d’en prendre connaissance.</p>
 
                 <h2>3. Description des services fournis.</h2>
 
-                <p>Le site internet <a href="https://salondechat.com">https://salondechat.com</a> a pour objet de fournir une information concernant l’ensemble des activités de la société.
+                <p>Le site internet <a href="https://salondechat.com">https://salondechat.com</a> a pour objet de fournir une information concernant l'ensemble des activités de la société.
                 <a href="https://salondechat.com">https://salondechat.com</a> s’efforce de fournir sur le site <a href="https://salondechat.com">https://salondechat.com</a> des informations aussi précises que possible. Toutefois, il ne pourra être tenu responsable des oublis, des inexactitudes et des carences dans la mise à jour, qu’elles soient de son fait ou du fait des tiers partenaires qui lui fournissent ces informations.</p>
 
-                <p>Toutes les informations indiquées sur le site <a href="https://salondechat.com">https://salondechat.com</a> sont données à titre indicatif, et sont susceptibles d’évoluer. Par ailleurs, les renseignements figurant sur le site <a href="https://salondechat.com">https://salondechat.com</a> ne sont pas exhaustifs. Ils sont donnés sous réserve de modifications ayant été apportées depuis leur mise en ligne.</p>
+                <p>Toutes les informations indiquées sur le site <a href="https://salondechat.com">https://salondechat.com</a> sont données à titre indicatif, et sont susceptibles d'évoluer. Par ailleurs, les renseignements figurant sur le site <a href="https://salondechat.com">https://salondechat.com</a> ne sont pas exhaustifs. Ils sont donnés sous réserve de modifications ayant été apportées depuis leur mise en ligne.</p>
 
                 <h2>4. Limitations contractuelles sur les données techniques.</h2>
 
                 <p>Le site utilise la technologie JavaScript.
 
-                Le site Internet ne pourra être tenu responsable de dommages matériels liés à l’utilisation du site. De plus, l’utilisateur du site s’engage à accéder au site en utilisant un matériel récent, ne contenant pas de virus et avec un navigateur de dernière génération mis-à-jour
-                Le site <a href="https://salondechat.com">https://salondechat.com</a> est hébergé chez un prestataire sur le territoire de l’Union Européenne conformément aux dispositions du Règlement Général sur la Protection des Données (RGPD : n° 2016-679)</p>
+                Le site Internet ne pourra être tenu responsable de dommages matériels liés à l'utilisation du site. De plus, l'utilisateur du site s'engage à accéder au site en utilisant un matériel récent, ne contenant pas de virus et avec un navigateur de dernière génération mis-à-jour
+                Le site <a href="https://salondechat.com">https://salondechat.com</a> est hébergé chez un prestataire sur le territoire de l'Union Européenne conformément aux dispositions du Règlement Général sur la Protection des Données (RGPD : n° 2016-679)</p>
 
-                <p>L’objectif est d’apporter une prestation qui assure le meilleur taux d’accessibilité. L’hébergeur assure la continuité de son service 24 Heures sur 24, tous les jours de l’année. Il se réserve néanmoins la possibilité d’interrompre le service d’hébergement pour les durées les plus courtes possibles notamment à des fins de maintenance, d’amélioration de ses infrastructures, de défaillance de ses infrastructures ou si les Prestations et Services génèrent un trafic réputé anormal.</p>
+                <p>L'objectif est d'apporter une prestation qui assure le meilleur taux d'accessibilité. L'hébergeur assure la continuité de son service 24 Heures sur 24, tous les jours de l'année. Il se réserve néanmoins la possibilité d’interrompre le service d’hébergement pour les durées les plus courtes possibles notamment à des fins de maintenance, d’amélioration de ses infrastructures, de défaillance de ses infrastructures ou si les Prestations et Services génèrent un trafic réputé anormal.</p>
 
                 <p><a href="https://salondechat.com">https://salondechat.com</a> et l’hébergeur ne pourront être tenus responsables en cas de dysfonctionnement du réseau Internet, des lignes téléphoniques ou du matériel informatique et de téléphonie lié notamment à l’encombrement du réseau empêchant l’accès au serveur.</p>
 
