0deaaad880816fb1e1cac8d520aed6e3cf0df2ba
unknown <hamedbelkhiria706@gmail.com>
2025-01-12 19:29:23 +0100

    m

--- server/models/usersCollectionStructure.js
+++ server/models/usersCollectionStructure.js
@@ -26,12 +26,12 @@ const subscriptionSchema = new mongoose.Schema({
   price: { type: Number, required: true },
 });
 //Encrypt password before saving
-/*
+ 
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) next();
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-});*/
+}); 
 
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
