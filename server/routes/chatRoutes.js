1b95f0d37b4315cf53b1bc6b333ea86f7f748a26
unknown <hamedbelkhiria706@gmail.com>
2025-01-16 08:07:29 +0100

    m

--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -11,13 +11,13 @@ const router = express.Router();
 
 router.post("/tcreatechatroom", createChatRoom);
 router.post("/tcreatechatroom1", createChatRoom1);
-router.put("/tchatroom/:id/creator", updateChatRoomCreatorPut);
+router.put("/tchatroom/:id/:creator", updateChatRoomCreatorPut);
 router.post("/tgetchatrooms", getChatrooms);
 
 router.use(authMiddleware);
 
 router.post("/createchatroom", createChatRoom);
 router.post("/createchatroom1", createChatRoom1);
-router.put("/chatroom/:id/creator", updateChatRoomCreatorPut); 
+router.put("/chatroom/:id/:creator", updateChatRoomCreatorPut); 
 router.post("/getchatrooms", getChatrooms);
 module.exports = router;
