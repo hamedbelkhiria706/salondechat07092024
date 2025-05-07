5ecb62369daa0dc6457c836d7ddea59f767a76c1
hamedbelkhiria706 <68014478+hamedbelkhiria706@users.noreply.github.com>
2025-05-07 11:33:56 +0200

    3
    f

--- src/components/FeatureCard.jsx
+++ src/components/FeatureCard.jsx
@@ -1,16 +1,22 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const FeatureCard = ({ title, description }) => {
   return (
     <div className="col-md-4 bg-dark">
-      <div className="card mb-4 bg-dark">
-        <div className="card-body bg-dark">
-          <h5 className="card-title bg-dark text-white">{title}</h5>
-          <p className="card-text bg-dark text-white">{description}</p>
+      <div className="card mb-4 bg-dark text-white">
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{description}</p>
         </div>
       </div>
     </div>
   );
+};
+
+FeatureCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
 };
 
 export default FeatureCard;
