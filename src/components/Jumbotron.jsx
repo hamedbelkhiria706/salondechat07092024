35686d53ae14351a19418d6b5c10f4b269057515
hamedbelkhiria706 <68014478+hamedbelkhiria706@users.noreply.github.com>
2025-05-08 15:36:34 +0200

    3
    3

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -1,20 +1,36 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const Jumbotron = () => {
+const Jumbotron = ({
+  title = "Bienvenue sur Notre Application de Chat",
+  leadText = "Discutez en toute simplicité avec vos amis, collègues et proches, où que vous soyez !",
+  buttonText = "Commencez à Chatter",
+  onButtonClick,
+}) => {
   return (
     <div className="jumbotron jumbotron-fluid text-center">
       <div className="container">
-        <h1 className="display-4">Bienvenue sur Notre Application de Chat</h1>
-        <p className="lead">
-          Discutez en toute simplicité avec vos amis, collègues et proches, où
-          que vous soyez !
-        </p>
-        <a href="#" className="btn btn-primary btn-lg mt-4">
-          Commencez à Chatter
-        </a>
+        <h1 className="display-4">{title}</h1>
+        <p className="lead">{leadText}</p>
+        {onButtonClick && (
+          <button
+            type="button"
+            className="btn btn-primary btn-lg mt-4"
+            onClick={onButtonClick}
+          >
+            {buttonText}
+          </button>
+        )}
       </div>
     </div>
   );
+};
+
+Jumbotron.propTypes = {
+  title: PropTypes.string,
+  leadText: PropTypes.string,
+  buttonText: PropTypes.string,
+  onButtonClick: PropTypes.func,
 };
 
 export default Jumbotron;
